af0d2be6a8a631d53e525fca2c3c8d5b8ce2706b
woo00oo <96woo94@naver.com>
2020-11-10 18:10:38 +0900

    user데이터-delete()추가

--- src/test/java/com/example/admin/repository/UserRepositoryTest.java
+++ src/test/java/com/example/admin/repository/UserRepositoryTest.java
@@ -2,9 +2,11 @@ package com.example.admin.repository;
 
 import com.example.admin.AdminApplicationTests;
 import com.example.admin.model.entity.User;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -16,11 +18,11 @@ public class UserRepositoryTest extends AdminApplicationTests {
     @Test
     public void create(){
         User user = new User();
-        user.setAccount("TestUser04");
+        user.setAccount("TestUser05");
         user.setEmail("Test@test.com");
         user.setPhoneNumber("010-1111-1111");
         user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("testUser4");
+        user.setCreatedBy("testUser5");
 
         User newUser = userRepository.save(user);
         System.out.print("newUser:"+newUser);
@@ -37,8 +39,9 @@ public class UserRepositoryTest extends AdminApplicationTests {
     }
 
     @Test
+    @Transactional
     public void update(){
-        Optional<User> user = userRepository.findById(2L);
+        Optional<User> user = userRepository.findById(3L);
 
         user.ifPresent(selectUser ->{
             selectUser.setAccount("ppppp");
@@ -50,7 +53,20 @@ public class UserRepositoryTest extends AdminApplicationTests {
         });
     }
 
+    @Test
+    @Transactional
     public void delete(){
+        Optional<User> user = userRepository.findById(3L);
+
+        Assert.assertTrue(user.isPresent()); //true
+
+        user.ifPresent(selectUser ->{
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assert.assertFalse(deleteUser.isPresent()); //false
 
     }
 
