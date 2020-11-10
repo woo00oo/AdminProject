b13d240a12a28d498c1628c8182dcf5424dd7bf6
woo00oo <96woo94@naver.com>
2020-11-10 14:09:40 +0900

    REST API

---
+++ src/main/java/com/example/admin/controller/PostController.java
@@ -0,0 +1,29 @@
+package com.example.admin.controller;
+
+import com.example.admin.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    //HTML <Form>
+    //ajax 검색
+    //http post body => data
+    //json, xml, multipart-form / text
+
+    @PostMapping(value = "/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+
+        return searchParam;
+    }
+
+    @PutMapping
+    public void put(){
+
+    }
+
+    public void patch(){
+        
+    }
+}
