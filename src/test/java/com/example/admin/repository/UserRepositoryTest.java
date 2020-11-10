a8dd2c192e676162e02ad009c437ce5ae3c1fa05
woo00oo <96woo94@naver.com>
2020-11-10 17:21:07 +0900

    User데이터 - read(), update() 메소드 추가

--- src/test/java/com/example/admin/repository/UserRepositoryTest.java
+++ src/test/java/com/example/admin/repository/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends AdminApplicationTests {
 
@@ -25,12 +26,28 @@ public class UserRepositoryTest extends AdminApplicationTests {
         System.out.print("newUser:"+newUser);
     }
 
+    @Test
     public void read(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            System.out.println("user:"+selectUser);
+            System.out.println("email : "+selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            selectUser.setAccount("ppppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+
+        });
     }
 
     public void delete(){
