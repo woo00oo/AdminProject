7b7d89f0df7140a403c8d558f805c5d5d78f3f9c
woo00oo <96woo94@naver.com>
2020-11-11 17:37:41 +0900

    item 엔터티 추가

---
+++ src/main/java/com/example/admin/model/entity/Item.java
@@ -0,0 +1,24 @@
+package com.example.admin.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+
+    private String content;
+
+}
