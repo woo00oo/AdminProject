b02b6eca5710572c2022631878dbc9f0d8b58a3f
woo00oo <96woo94@naver.com>
2020-11-14 09:27:25 +0900

    Item entity 수정

--- src/main/java/com/example/admin/model/entity/Item.java
+++ src/main/java/com/example/admin/model/entity/Item.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -17,18 +18,43 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String status;
+
     private String name;
 
-    private Integer price;
+    private String title;
 
     private String content;
 
-    // 1 : n
+    private Integer price;
 
-    // LAZY = 지연 로딩(조인이 필요한 메서드를 호출하는 경우 조인 시작) 1:n, n:1, n:m
-    // EAGER = 즉시 로딩(연관 관계가 설정된 모든 속성에 대한 조인이 일어남) 1:1
+    private String brandName;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
-    private List<OrderDetail> orderDetailList;
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+
+
+
+
+
+
+
+//    // 1 : n
+//
+//    // LAZY = 지연 로딩(조인이 필요한 메서드를 호출하는 경우 조인 시작) 1:n, n:1, n:m
+//    // EAGER = 즉시 로딩(연관 관계가 설정된 모든 속성에 대한 조인이 일어남) 1:1
+//
+//    @OneToMany(fetch = FetchType.LAZY, mappedBy = "item")
+//    private List<OrderDetail> orderDetailList;
 
 }
