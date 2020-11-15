3e8f3ed169b201973d1b901f2649eb5ae8fc1276
woo00oo <96woo94@naver.com>
2020-11-15 13:49:31 +0900

    User Create API 생성

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -2,28 +2,41 @@ package com.example.admin.controller.api;
 
 import com.example.admin.ifs.CrudInterface;
 import com.example.admin.model.network.Header;
+import com.example.admin.model.network.request.UserApiRequest;
+import com.example.admin.model.network.response.UserApiResponse;
+import com.example.admin.service.UserApiLogicService;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("")
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        //log.info("{},{}",request,"ABC"); // -> request.toString(), ABC
+        log.info("{}",request);
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}")
-    public Header read(@PathVariable(name = "id") Long id) {
+    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
     @PutMapping("")
-    public Header update() {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> userApiRequest) {
         return null;
     }
 
