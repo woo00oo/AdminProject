7671a720ef09968e804b8b9e9351b61472085499
woo00oo <96woo94@naver.com>
2020-11-29 20:11:03 +0900

    ItemApiController 추상화

--- src/main/java/com/example/admin/controller/api/ItemApiController.java
+++ src/main/java/com/example/admin/controller/api/ItemApiController.java
@@ -1,5 +1,6 @@
 package com.example.admin.controller.api;
 
+import com.example.admin.controller.CrudController;
 import com.example.admin.ifs.CrudInterface;
 import com.example.admin.model.network.Header;
 import com.example.admin.model.network.request.ItemApiRequest;
@@ -8,35 +9,18 @@ import com.example.admin.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-
-    @Override
-    @PostMapping("")
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct
+    public void init(){
+        this.baseService = itemApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
-    }
 }
