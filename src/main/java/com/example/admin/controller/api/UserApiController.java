8d6b1b0371a616374f9890d95739a2f2de2af4d9
woo00oo <96woo94@naver.com>
2020-12-01 08:56:32 +0900

    UserController 페이징 처리

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -10,7 +10,13 @@ import com.example.admin.service.UserApiLogicService;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,6 +25,11 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Autowired
     private UserApiLogicService userApiLogicService;
+
+    @GetMapping("")
+    public Header<List<UserApiResponse>> search(@PageableDefault(sort="id",direction = Sort.Direction.ASC,size=15) Pageable pageable){
+        return userApiLogicService.search(pageable);
+    }
 
     @Override
     @PostMapping("")
