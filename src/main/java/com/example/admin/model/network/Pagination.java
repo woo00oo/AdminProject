7a7d919863a69cfbd7c9a771baf7c05dd5f76f7f
woo00oo <96woo94@naver.com>
2020-12-01 08:57:48 +0900

    페이징 처리클래스 멤버변수 설정

--- src/main/java/com/example/admin/model/network/Pagination.java
+++ src/main/java/com/example/admin/model/network/Pagination.java
@@ -1,4 +1,21 @@
 package com.example.admin.model.network;
 
-public class Pagenation {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class Pagination {
+
+    private Integer totalPages;
+    private Long totalElements;
+    private Integer currentPage;
+    private Integer currentElements;
+
 }
