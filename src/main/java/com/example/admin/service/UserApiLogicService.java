800cac3ecfbdc273af9015dc5244f0fe6c90dd16
woo00oo <96woo94@naver.com>
2020-12-01 08:58:11 +0900

    유저서비스 페이징처리 추가

--- src/main/java/com/example/admin/service/UserApiLogicService.java
+++ src/main/java/com/example/admin/service/UserApiLogicService.java
@@ -4,14 +4,19 @@ import com.example.admin.ifs.CrudInterface;
 import com.example.admin.model.entity.User;
 import com.example.admin.model.enumclass.UserStatus;
 import com.example.admin.model.network.Header;
+import com.example.admin.model.network.Pagination;
 import com.example.admin.model.network.request.UserApiRequest;
 import com.example.admin.model.network.response.UserApiResponse;
 import com.example.admin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
@@ -38,7 +43,7 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
         User newUser = userRepository.save(user);
 
         //3. 생성된 데이터 -> UserApiResponse return
-        return response(newUser);
+        return Header.OK(response(newUser));
     }
 
     @Override
@@ -46,6 +51,8 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
         return userRepository.findById(id)
                 .map(user -> response(user))
+                .map(userApiResponse -> Header.OK(userApiResponse))
+                //.map(Header::OK)
                 .orElseGet(
                         ()->Header.ERROR("데이터 없음")
                 );
@@ -74,6 +81,7 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
         })
         .map(user -> userRepository.save(user)) //3. update
         .map(updateUser -> response(updateUser)) //4. userApiResponse
+        .map(Header::OK)
         .orElseGet(()->Header.ERROR("데이터 없음"));
 
     }
@@ -96,7 +104,7 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
 
     }
 
-    private Header<UserApiResponse> response(User user){
+    private UserApiResponse response(User user){
         // user -> userApiResponse
 
         UserApiResponse userApiResponse = UserApiResponse.builder()
@@ -111,6 +119,24 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
                 .build();
         // Header + data return
 
-        return Header.OK(userApiResponse);
+        return userApiResponse;
+    }
+
+    public Header<List<UserApiResponse>> search(Pageable pageable) {
+
+        Page<User> users = userRepository.findAll(pageable);
+
+        List<UserApiResponse> userApiResponseList = users.stream()
+                .map(user -> response(user))
+                .collect(Collectors.toList());
+
+        Pagination pagination = Pagination.builder()
+                .totalPages(users.getTotalPages())
+                .totalElements(users.getTotalElements())
+                .currentPage(users.getNumber())
+                .currentElements(users.getNumberOfElements())
+                .build();
+
+        return Header.OK(userApiResponseList,pagination);
     }
 }
